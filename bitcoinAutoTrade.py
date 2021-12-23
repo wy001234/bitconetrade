4d853d22ab6ddc63e8a668a3ec265426978199c2
wy001234 <96578256+wy001234@users.noreply.github.com>
2021-12-24 05:36:34 +0900

    Update bitcoinAutoTrade.py

--- bitcoinAutoTrade.py
+++ bitcoinAutoTrade.py
@@ -45,7 +45,7 @@ while True:
 
         #9시부터 <현재 < #8시 59분 50초
         if start_time < now < end_time - datetime.timedelta(seconds=10):
-            target_price = get_target_price("KRW-BTC", 0.5)
+            target_price = get_target_price("KRW-BTC", 0.7)
             current_price = get_current_price("KRW-BTC")
             if target_price < current_price:
                 krw = get_balance("KRW")
